ca5c0bf50ba38d4b78f34dfd7999bbe911de3a6d
andreavaughan <amvaughan16@gmail.com>
2021-07-20 18:07:58 -0400

    completed variables for 1st customer card

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ const customerCard = document.createElement('div')
 customerCard.classList.add('customer-card')
 // customerCard.setAttribute('class', 'customer-card')
 
+outputDiv.appendChild(customerCard)
+
 const firstName = document.createElement('h1')
-firstName.innerText = customers[0].name.first
+firstName.innerText = customers[0].name.first //refactor firstName and lastName to be one variable fullName
 
 console.log(firstName)
 
 const lastName = customers[0].name.last
 firstName.innerText += ' ' + lastName
 customerCard.appendChild(firstName)
-outputDiv.appendChild(customerCard)
 
 const customerPic = document.createElement('img')
 customerPic.src = customers[0].picture.medium //remember to comeback and change img size when refactoring 
@@ -27,8 +28,21 @@ customerEmail.appendChild(link)
 customerEmail.href = customers[0].email
 customerEmail.target = "_blank"
 
-
-// customerEmail.link = customers[0].email
-
 outputDiv.appendChild(customerEmail)
+
+const customerAddress1 = document.createElement('address')
+customerAddress1.innerText = customers[0].location.street.number + ' ' + customers[0].location.street.name
+outputDiv.appendChild(customerAddress1)
+
+const customerAddress2 = document.createElement('address')
+customerAddress2.innerText = customers[0].location.city + ', ' + customers[0].location.state + ' ' + customers[0].location.postcode
+outputDiv.appendChild(customerAddress2)
+
+const customerBirthday = document.createElement('div')
+customerBirthday.innerText = customers[0].dob.date
+outputDiv.appendChild(customerBirthday)
+
+const customerSince = document.createElement('div')
+customerSince.innerText = customers[0].registered.date
+outputDiv.appendChild(customerSince)
 
