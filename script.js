fa7f3d8dd06e5de67fcc354c7e414b785525b8b2
andreavaughan <amvaughan16@gmail.com>
2021-07-20 21:44:12 -0400

    updated state abbr

--- script.js
+++ script.js
@@ -10,11 +10,7 @@ const outputDiv = document.getElementById('root')
 
 populateCustomerCard(customers);
 
-//test array object 
 
-// let testCustomers =
-
-//function purpose: to create a div that populates customer information as called
 
 function populateCustomerCard(customers){
     for (i = 0; i < customers.length; i++){
@@ -30,8 +26,6 @@ function populateCustomerCard(customers){
             const firstName = document.createElement('h1')
             firstName.innerText = customers[i].name.first //refactor firstName and lastName to be one variable fullName
 
-            console.log(firstName)
-
             const lastName = customers[i].name.last
             firstName.innerText += ' ' + lastName
             customerCard.appendChild(firstName)
@@ -41,7 +35,6 @@ function populateCustomerCard(customers){
             customerEmail.appendChild(link)
             customerEmail.href = customers[i].email
             customerEmail.target = "_blank"
-
             customerCard.appendChild(customerEmail)
 
             const customerAddress1 = document.createElement('address')
@@ -49,18 +42,19 @@ function populateCustomerCard(customers){
             customerCard.appendChild(customerAddress1)
 
             const customerAddress2 = document.createElement('address')
-            customerAddress2.innerText = customers[i].location.city + ', ' + customers[i].location.state + ' ' + customers[i].location.postcode
+            customerAddress2.innerText = customers[i].location.city + ', ' + nameToAbbr(customers[i].location.state) + ' ' + customers[i].location.postcode
             customerCard.appendChild(customerAddress2)
 
             const customerBirthday = document.createElement('div')
-            customerBirthday.innerText = customers[i].dob.date
+            customerBirthday.innerText = 'DOB: ' + customers[i].dob.date
             customerCard.appendChild(customerBirthday)
 
             const customerSince = document.createElement('div')
-            customerSince.innerText = customers[i].registered.date
+            customerSince.innerText = 'Customer since: ' + customers[i].registered.date
             customerCard.appendChild(customerSince)
 
         }
     }
 }
-    
+
+
